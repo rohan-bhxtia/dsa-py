e87ea287f7859191e0b32b3c2a7961a18cc32c3e
Rohan ! <149778795+rohan-bhxtia@users.noreply.github.com>
2025-12-30 23:54:19 +0530

    Update 7Get.py

--- LL/7Get.py
+++ LL/7Get.py
@@ -2,7 +2,7 @@ class Node:
     def __init__(self,value):
         self.value = value
         self.next = None
-
+.
 class linkedlist:
     def __init__(self,value):
         new_node = Node(value)
