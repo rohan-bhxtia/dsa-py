33259006b1b0ad0f53939f7dc1115b3f84aee3d6
Rohan ! <149778795+rohan-bhxtia@users.noreply.github.com>
2026-01-04 22:43:19 +0530

    Update 1LinkedList.py

--- LL/1LinkedList.py
+++ LL/1LinkedList.py
@@ -2,7 +2,7 @@ class Node:
     def __init__(self,value):
         self.value = value
         self.next = None
-
+.
 class LinkedList:
     def __init__(self,value):
         new_node = Node(value)
